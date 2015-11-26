c9d0147e27925961616f2f3dfe70fb63886d954d
cunshuifengyun <wangshixiebo@126.com>
2015-11-26 13:55:53 +0800

    dd
    dd

--- models/WomanCollection.js
+++ models/WomanCollection.js
@@ -4,8 +4,13 @@
 var mongodb = require('./mongodb');
 var Schema = mongodb.mongoose.Schema;
 var WomanSchema = new Schema({
-    name: String,
-    age: Number
+    name: {
+        type: 'String',
+        required: true
+    },
+    age: {
+        type: 'Number'
+    }
 },{
     collection: 'women'
 });
@@ -26,11 +31,26 @@ exports.create = function(woman) {
 exports.retrieve = function() {
 
 };
-exports.update = function() {
+exports.update = function(id,woman) {
 
+    WomanModel.update({_id:id},woman,function(err,raw) {
+        if(err) {
+            return console.error(err);
+        }
+        console.log("The raw response from Mongo was", raw);
+    });
 };
 exports.delete = function() {
 
+};
+exports.findAll = function(callback) {
+  WomanModel.find({},function(err,docs) {
+      if(err) {
+          console.log('FATAL ' + err);
+      }
+      console.log(docs);
+      callback(docs);
+  });
 };
 //清空表
 function emptyCollection() {
--- routes/woman.js
+++ routes/woman.js
@@ -1,19 +1,48 @@
 /**
  * Created by DeepBlue on 2015/11/24.
  */
-var Woman = require('./../models/Woman.js');
+var WomanCollection = require('./../models/WomanCollection.js');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/',function(req, res, next) {
     res.send('respond with a resource');
 });
-router.post('/woman/new', function(req, res) {
+router.post('/woman/create',function(req, res) {
+    var woman = req.body;
+    WomanCollection.create(woman);
+    res.sendStatus(200);
+
 
 });
-router.post('/woman/modify', function(req, res) {
+router.post('/woman/retrieve/:id',function(req, res,next) {
+    var id = req.params.id;
+    if(id === 'all') {
+        return next();
+    }else {
+        console.log("通过ID查询！");
+    }
 
+    res.sendStatus(200);
+
+
+});
+router.post('/woman/retrieve/all',function(req,res) {
+    console.log("查询所有！");
+    var datas = WomanCollection.findAll(function(docs) {
+       res.send(docs);
+    });
+});
+router.post('/woman/update/:id',function(req, res) {
+    var id = req.params.id;
+    var woman = req.body;
+
+    WomanCollection.update(id,woman);
+    res.send(woman);
+});
+router.delete('/woman/delete/:id',function(req,res) {
+    res.sendStatus(200);
 });
 
 module.exports = router;
